f9e82787c6822e79fbb2096c5f6b8a13eec53a5b
Yeti Sno <yeti@yetiz.org>
2021-05-12 15:57:01 +0800

    add cassandra

---
+++ cassandra.go
@@ -0,0 +1,113 @@
+package datastore
+
+import (
+	"sync"
+
+	"github.com/gocql/gocql"
+	"github.com/kklab-com/goth-kklogger"
+	kksecret "github.com/kklab-com/goth-kksecret"
+)
+
+var KKCassandraLocker = sync.Mutex{}
+var KKCassandraProfiles = sync.Map{}
+var KKCassandraDebug = false
+var _KKCassandraDebug = sync.Once{}
+
+type Cassandra struct {
+	name   string
+	writer *CassandraOp
+	reader *CassandraOp
+}
+
+func (k *Cassandra) Writer() *CassandraOp {
+	return k.writer
+}
+
+func (k *Cassandra) Reader() *CassandraOp {
+	return k.reader
+}
+
+type CassandraOp struct {
+	opType  OPType
+	meta    kksecret.CassandraMeta
+	cluster *gocql.ClusterConfig
+	session *gocql.Session
+	once    sync.Once
+}
+
+func (k *CassandraOp) Cluster() *gocql.ClusterConfig {
+	return k.cluster
+}
+
+func (k *CassandraOp) Session() *gocql.Session {
+	k.once.Do(func() {
+		session, err := k.cluster.CreateSession()
+		if err != nil {
+			kklogger.ErrorJ("goth-kkdatastore:CassandraOp.Session", err.Error())
+		}
+
+		k.session = session
+	})
+
+	return k.session
+}
+
+func KKCassandra(cassandraName string) *Cassandra {
+	_KKCassandraDebug.Do(func() {
+		if !KKCassandraDebug {
+			KKCassandraDebug = _IsKKDatastoreDebug()
+		}
+	})
+
+	if r, f := KKCassandraProfiles.Load(cassandraName); f && !KKCassandraDebug {
+		return r.(*Cassandra)
+	}
+
+	profile := kksecret.CassandraProfile(cassandraName)
+	if profile == nil {
+		return nil
+	}
+
+	KKCassandraLocker.Lock()
+	defer KKCassandraLocker.Unlock()
+	if KKCassandraDebug {
+		KKCassandraProfiles.Delete(cassandraName)
+	}
+
+	if r, f := KKCassandraProfiles.Load(cassandraName); !f {
+		csd := &Cassandra{
+			name: cassandraName,
+		}
+
+		wop := &CassandraOp{}
+		wop.opType = TypeWriter
+		wop.meta = profile.Writer
+		wop.cluster = gocql.NewCluster(wop.meta.Hosts...)
+		wop.cluster.Authenticator = gocql.PasswordAuthenticator{
+			Username: wop.meta.Username,
+			Password: wop.meta.Password,
+		}
+
+		wop.cluster.SslOpts = &gocql.SslOptions{CaPath: wop.meta.CaPath}
+		wop.cluster.Consistency = gocql.LocalQuorum
+		csd.writer = wop
+
+		rop := &CassandraOp{}
+		rop.opType = TypeReader
+		rop.meta = profile.Reader
+		rop.cluster = gocql.NewCluster(rop.meta.Hosts...)
+		rop.cluster.Authenticator = gocql.PasswordAuthenticator{
+			Username: rop.meta.Username,
+			Password: rop.meta.Password,
+		}
+
+		rop.cluster.SslOpts = &gocql.SslOptions{CaPath: rop.meta.CaPath}
+		rop.cluster.Consistency = gocql.LocalQuorum
+		csd.reader = rop
+
+		KKCassandraProfiles.Store(cassandraName, csd)
+		return csd
+	} else {
+		return r.(*Cassandra)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.16
 
 require (
 	cloud.google.com/go v0.38.0 // indirect
+	github.com/gocql/gocql v0.0.0-20210504150947-558dfae50b5d // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/go-cmp v0.3.0 // indirect
 	github.com/jinzhu/gorm v1.9.11
-	github.com/kklab-com/goth-kklogger v0.0.0-20210329100034-0d99b494f9a5
-	github.com/kklab-com/goth-kksecret v0.0.0-20210329095243-96b2cff3031d
-	github.com/stretchr/testify v1.3.0
+	github.com/kklab-com/goth-kklogger v0.0.0-20210510033105-1f4bb2108d4e
+	github.com/kklab-com/goth-kksecret v0.0.0-20210512070445-b3ad95e5b5fd
+	github.com/stretchr/testify v1.7.0
 	google.golang.org/appengine v1.5.0 // indirect
 )
